89fbe5e977f806fb40678496aa5c03e2a4a95e73
zhljian <zhljian@bainainfo.com>
2017-06-13 10:53:26 +0800

    add command

--- example.js
+++ example.js
@@ -1,6 +1,21 @@
 let compression = require('./bnImageCompression');
+let program = require('commander');
 
-compression.compressionDir('D:/zhljian/work/tinify').then(function(){
-    console.log('success');
-});
+program
+    .version('0.0.1')
+    .option('-o, --outuput', 'Output path')
+    .option('-i, --input', 'Input path')
+    .parse(process.argv)
+
+let inputPath = program.input;
+//'D:/zhljian/work/tinify'
+if (!inputPath) {
+    console.log('input error');
+} else {
+    compression.compressionDir('D:/zhljian/work/tinify').then(function(){
+        console.log('success');
+    });
+}
+
+
 
