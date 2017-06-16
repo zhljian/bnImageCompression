3678a598911127719c8e48e1785945a8222a2091
zhljian <zhljian@bainainfo.com>
2017-06-16 17:32:05 +0800

    add keyfile

--- example/example.js
+++ example/example.js
@@ -1,6 +1,6 @@
 let compression = require('../index');
 
-compression.compress("../game-js-slots/build/jsb-default/res/raw-assets/resources/10001/", null, 'mWOnZaTbJTNARWKPy8mdgf1Ju3cROIjU', function(error){
+compression.compress("../../game-js-slots/build/jsb-default/res/raw-assets/resources/10001/", null, '../key.txt', function(error){
     if (error) {
         console.error(error);
         return ;
